85de044ceca789194d7a408ed5dfd73d568534df
shruti611 <61108858+shruti611@users.noreply.github.com>
2024-02-25 19:52:23 +0530

    Add files via upload

--- RC/RecipesTableViewController.swift
+++ RC/RecipesTableViewController.swift
@@ -42,7 +42,19 @@ class RecipesTableViewController: UITableViewController {
         return cell
     }
     
-
+    
+   
+    @IBSegueAction func showDetailedRecipe(_ coder: NSCoder, sender: Any?) -> RecipeDetailTableViewController? {
+        
+        if let cell = sender as? RecipeTableViewCell, let indexPath = tableView.indexPath(for: cell){
+            let recipe = recipes[indexPath.row]
+            return RecipeDetailTableViewController(coder: coder, recipe: recipe)
+        }
+        return RecipeDetailTableViewController(coder: coder, recipe: nil)
+            
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
